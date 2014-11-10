7ed1a791011114d96cca547ba3b65b2657c026f9
Tyler <Tyler.A.Staples@gmail.com>
2014-11-10 00:20:32 -0800

    Removed sources files that were moved in previous commit

--- source/PerspectiveMatrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDED_PERSPECTIVEMATRIX_H
-#define INCLUDED_PERSPECTIVEMATRIX_H
-
-class CMatrix44;
-
-CMatrix44 ComputeProjectionMatrix(float FOVDegrees, float fAspectRatio, float fNear, float fFar);
-
-
-#endif // ifndef INCLUDED_PERSPECTIVEMATRIX_H
